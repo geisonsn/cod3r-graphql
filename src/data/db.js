674166beae80a4c2b38429cb663a6e74bf16b89d
Geison Santos <geison.sn@gmail.com>
2019-08-12 10:19:28 -0400

    Adiciona o uso de ENUM no tipo de Usuario

--- src/data/db.js
+++ src/data/db.js
@@ -9,21 +9,24 @@ const usuarios = [
     nome: 'Bernardo Cavalcante do Nascimento',
     email: 'bernardocn@gmail.com',
     idade: 20,
-    perfil_id: 1
-  },
-  {
+    perfil_id: 1,
+    status: 'ATIVO' 
+  }, 
+  { 
     id: 2,
     nome: 'Maria de Albuquerque',
     email: 'malbuquerque@gmail.com',
     idade: 30,
-    perfil_id: 2
-  },
-  {
-    id: 3,
+    perfil_id: 2,
+    status: 'INATIVO'
+  }, 
+  { 
+    id: 3, 
     nome: 'José Maria da Silva',
     email: 'jmsilva@gmail.com',
     idade: 39,
-    perfil_id: 1
+    perfil_id: 1,
+    status: 'BLOQUEADO'
   }
 ];
 
